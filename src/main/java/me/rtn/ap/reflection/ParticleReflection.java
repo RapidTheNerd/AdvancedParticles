ede6f122b19479245f4b796576d101b4da546d1e
RapidTheNerd <george.marr.cobc@gmail.com>
2017-04-05 00:45:41 +0100

    fucking parameters

--- src/main/java/me/rtn/ap/reflection/ParticleReflection.java
+++ src/main/java/me/rtn/ap/reflection/ParticleReflection.java
@@ -1,7 +1,9 @@
-package me.rtn.ap;
+package me.rtn.ap.reflection;
 
+import me.rtn.ap.Main;
 import org.bukkit.entity.Player;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -10,6 +12,9 @@ import java.lang.reflect.Method;
  * Created by George on 05-Apr-17 on Apr at 12:36 AM.
  */
 public class ParticleReflection {
+
+    public ParticleReflection() throws ClassNotFoundException, NoSuchMethodException {
+    }
 
     private Class<?> getNMSClass(String nmsClass) throws ClassNotFoundException{
         String ver = Main.getInstance().getServer().getClass().getPackage().getName().replace(".", "")
@@ -28,4 +33,12 @@ public class ParticleReflection {
 
         return con;
     }
+    public void sendParticle(Player player, String particle, float x, float y, float z, float xOffset, float zOffset,
+                             float yOffzet, float data, int amount){
+
+    }
+
+    Class<?> packetClass = this.getNMSClass("PacketPlayOutWorldParticles");
+    Constructor<?> packetConstructor = packetClass.getConstructor(String.class, float.class,
+            float.class, float.class, float.class, float.class, float.class, float.class, int.class);
 }
