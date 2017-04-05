b68d9a6cea649581c3d567b34e6459c0d78120ca
RapidTheNerd <george.marr.cobc@gmail.com>
2017-04-05 01:11:47 +0100

    thats probably why it isn't working£

--- src/main/java/me/rtn/ap/SomeGlobeThing.java
+++ src/main/java/me/rtn/ap/SomeGlobeThing.java
@@ -8,32 +8,29 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.util.Vector;
 
 /**
  * Created by George on 05-Apr-17 on Apr at 12:53 AM.
  */
 public class SomeGlobeThing implements Listener {
 
-    private void activateParticle(PlayerInteractEvent event){
+    public void activateParticle(PlayerInteractEvent event){
 
         Player player = event.getPlayer();
         Location loc = player.getLocation();
-        Vector vec  = loc.getDirection().normalize();
         World world = Main.getInstance().getServer().getWorld("world");
 
         if(event.getAction() == Action.LEFT_CLICK_AIR){
             new BukkitRunnable() {
 
                 double phi = 0;
-                double theta;
 
                 @Override
                 public void run() {
 
                     phi *= Math.PI /10;
 
-                    for(theta = 0; theta <= 2*Math.PI; theta+= Math.PI / 40){
+                    for(double theta = 0; theta <= 2*Math.PI; theta+= Math.PI / 40){
                         double radius = 1.5;
                         double x = radius*Math.cos(theta) * Math.sin(phi);
                         double y = radius*Math.cos(phi) + 1.5;
@@ -41,9 +38,10 @@ public class SomeGlobeThing implements Listener {
 
                         loc.add(x,y,z);
                         world.spawnParticle(Particle.DRIP_WATER, loc, 0, 0, 0, 0, 1);
-                    }
-                    if(theta > Math.PI){
-                        this.cancel();
+
+                        if(theta > Math.PI){
+                            this.cancel();
+                        }
                     }
                 }
             }.runTaskTimer(Main.getInstance(), 0, 1);
